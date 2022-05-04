ceffdddc94a827da3a1d2d383e192eae1656ce68
uttamsaha <uttamsaha982@gmail.com>
2022-05-04 21:38:42 +0600

    delete product api added

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ async function run(){
         res.send(product);
       });
 
+      //delete
+      app.delete('/products/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await productsCollection.deleteOne(query);
+        res.send(result);
+    });
+
 }
 run().catch(console.dir);
 
