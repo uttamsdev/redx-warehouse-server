6c369cbc8856faff6bb713385edfcf226fba074b
uttamsaha <uttamsaha982@gmail.com>
2022-05-04 19:05:38 +0600

    data read by id done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const req = require("express/lib/request");
 const res = require("express/lib/response");
 const port = process.env.PORT || 5000;
@@ -28,7 +28,14 @@ async function run(){
         const cursor = productsCollection.find(query);
         const products = await cursor.toArray();
         res.send(products);
-    })
+    });
+
+    app.get("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const product = await productsCollection.findOne(query);
+        res.send(product);
+      });
 
 }
 run().catch(console.dir);
